c55f43b602a8103b2070460f8e1fdfe6ffd2fe2f
Roger <rmartinez5926.rm@gmail.com>
2020-04-23 12:02:19 -0400

    delete list from db when the button is clicked

--- public/js/grocery.js
+++ public/js/grocery.js
@@ -1,11 +1,11 @@
 // Once the document loads
 $(document).ready(function () {
   // When the form is submitted
-  $(".make-list").on("submit", function(event) {
+  $(".make-list").on("submit", function (event) {
     // prevent page reload
     event.preventDefault();
     // Get the users id
-    $.get("/auth/user" ,function(data) {
+    $.get("/auth/user", function (data) {
       // Create an object with the information from the form and the users id
       const List = {
         name: $("#name").val().trim(),
@@ -13,17 +13,27 @@ $(document).ready(function () {
         UserId: data.id
       }
       // Post new list to DB
-      $.post("/api/newList", List, function() {
+      $.post("/api/newList", List, function () {
         // Reload the page
-         window.location.reload();
+        window.location.reload();
       })
     })
-    
+
   });
   //delete button function
-  $(".delete-list").click(function(event){
+  $(".delete-list").click(function (event) {
+    // Prevent page reload
     event.preventDefault();
+    // Get the list id
     const id = $(this).data("listid");
-    console.log(id);
+    // do a delete to the database
+    $.ajax({ url: "/api/grocery-list/" + id, method: "DELETE" })
+      .then(function (data) {
+        //reload the page
+        window.location.reload();
+      })
+      .catch(function (err) {
+        if (err) throw err;
+      });
   })
 });
