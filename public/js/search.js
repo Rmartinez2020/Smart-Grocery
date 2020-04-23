e4bcc5f6451272158f2818c3267a93684becb40a
Roger <rmartinez5926.rm@gmail.com>
2020-04-23 09:49:02 -0400

    fix bug on registration page

--- public/js/search.js
+++ public/js/search.js
@@ -3,15 +3,21 @@ $(document).ready(function () {
     $(".search").on("submit", function (event) {
         // get the value from the input
         event.preventDefault();
+        //empty search data
         $(".recipes").empty();
         const param = $("#search-bar").val().trim();
+        // Do a get using the param
         $.get("/api/recipe/" + param, data => {
             console.log(data);
-            for (let index = 0; index < data.length; index++) {
+            //put the data on the page
+            for (let i = 0; i < data.length; i++) {
                 let cardDiv = $(`<div class="card my-4">`);
-                let cardHeader = $(`<div class="card-header text-center">${data[index].name.toUpperCase()}</div>`);
-                let cardBody = $(`<div class="card-body">${data[index].description}</div>`);
-                cardDiv = cardDiv.append(cardHeader, cardBody);
+                let cardHeader = $(`<div class="card-header text-center">${data[i].name.toUpperCase()}</div>`);
+                let cardBody = $(`<div class="card-body">`);
+                let description = $(`<p> Description:</br> ${data[i].description.toUpperCase()}</p>`);
+                let time = $(`<p> Time to make: ${data[i].minutes} minutes</p>`);
+                cardBody.append(description,time);
+                cardDiv.append(cardHeader, cardBody);
                 $(".recipes").append(cardDiv);
             }
         })
