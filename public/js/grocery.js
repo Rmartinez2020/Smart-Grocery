edd6375af7d50b8e7df684d572ac782314d184f7
Roger <rmartinez5926.rm@gmail.com>
2020-04-23 15:40:35 -0400

    fix update list

--- public/js/grocery.js
+++ public/js/grocery.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     // Set Global Variables
     const url = window.location.search;
-    const updatingList = false;
+    let updatingList = false;
     let listId;
     const name = $("#name");
     // Handle button click
-    $(".make-list").on("submit",handleSubmit(event))
+    $(".make-list").on("submit", handleSubmit)
 
     if (url.indexOf("?list-id=") !== -1) {
         //set the list id
@@ -17,32 +17,37 @@ $(document).ready(function () {
     function handleSubmit(event) {
         // prevent page reload
         event.preventDefault();
-         // Create an object with the information from the form and the users id
-         const List = {
+        // Create an object with the information from the form
+        const List = {
             name: $("#name").val().trim(),
             items: $("#items").val().trim(),
-            UserId: data.id
         }
-        if(updatingList){
-            $.update("/")
-        }
-        // Get the users id
-        $.get("/auth/user", function (data) {
+        if (updatingList) {
+            //get the user id and add it to object
+            $.get("/auth/user").then(function (data) {
+                List.UserId = data.id
+                $.post("/api/groceries/" + listId, List, function () {
+                    console.log("List Updated");
+                    window.location.assign("/");
+                })
+            })
+        } else {
             // Post new list to DB
             $.post("/api/newList", List, function () {
                 // Reload the page
                 window.location.reload();
             })
-        })
+        }
+
     };
     // Get one grocery list
     function getGroceryList(id) {
-        $.get("/api/groceries/"+id , list => {
+        $.get("/api/groceries/" + id, list => {
             //put the data in th form
             name.val(list.name);
             $("#items").val(list.items);
             //set updating to true
-           updatingList = true;
+            updatingList = true;
         })
     }
 })
