e0ce784bcc4fc2fc143b2b5685556934a43b2861
Roger <rmartinez5926.rm@gmail.com>
2020-04-23 13:01:40 -0400

    edit button goes to new page when clicked

--- public/js/grocery.js
+++ public/js/grocery.js
@@ -36,4 +36,12 @@ $(document).ready(function () {
         if (err) throw err;
       });
   })
+  // when edit button clicked
+  $(".edit-list").click(function (event) {
+    event.preventDefault();
+    // Grab the id
+    const id = $(this).data("listid");
+    // Go to make a list page
+    window.location.assign("/add-recipe?list-id="+id);
+  })
 });
