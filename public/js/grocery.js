ab946b523c0864293b96e7e95a2e2855c3dc4f4e
Roger <rmartinez5926.rm@gmail.com>
2020-04-23 15:16:06 -0400

    create a new handle function on grocery.js

--- public/js/grocery.js
+++ public/js/grocery.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     const updatingList = false;
     let listId;
     const name = $("#name");
+    // Handle button click
+    $(".make-list").on("submit",handleSubmit(event))
 
     if (url.indexOf("?list-id=") !== -1) {
         //set the list id
@@ -12,31 +14,35 @@ $(document).ready(function () {
         getGroceryList(listId);
     }
     // When the form is submitted
-    $(".make-list").on("submit", function (event) {
+    function handleSubmit(event) {
         // prevent page reload
         event.preventDefault();
+         // Create an object with the information from the form and the users id
+         const List = {
+            name: $("#name").val().trim(),
+            items: $("#items").val().trim(),
+            UserId: data.id
+        }
+        if(updatingList){
+            $.update("/")
+        }
         // Get the users id
         $.get("/auth/user", function (data) {
-            // Create an object with the information from the form and the users id
-            const List = {
-                name: $("#name").val().trim(),
-                items: $("#items").val().trim(),
-                UserId: data.id
-            }
             // Post new list to DB
             $.post("/api/newList", List, function () {
                 // Reload the page
                 window.location.reload();
             })
         })
-    });
+    };
     // Get one grocery list
     function getGroceryList(id) {
         $.get("/api/groceries/"+id , list => {
+            //put the data in th form
             name.val(list.name);
             $("#items").val(list.items);
-            console.log(list.name);
-            console.log(list.items);
+            //set updating to true
+           updatingList = true;
         })
     }
 })
