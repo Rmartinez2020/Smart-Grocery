4a3450827aa27c93c2c441f546f1af6f091ef691
Roger <rmartinez5926.rm@gmail.com>
2020-04-24 02:27:11 -0400

    create a card for ingredients

--- public/js/view.js
+++ public/js/view.js
@@ -3,7 +3,47 @@ $(document).ready(function () {
 
     $.get("/api/one-recipe/" + id, recipe => {
         console.log(recipe);
-        let name = $(`<h1>${recipe[0].name}</h1>`)
-        $(".recipe-view").append(name)
+        // create ingredients card
+
+        // make ingredients an array
+        let newIngredients = []
+        let ingredients = recipe[0].ingredients.split(",");
+        ingredients.forEach(element => {
+            newIngredients.push(element.replace("[", "").replace("]", "").replace("'", "").replace("'", ""));
+        });
+        let ingCard = $(`<div class="card my-4">`);
+        let cardHeader = $(`<div class="card-header">Ingredients:</div>`);
+        let ingList = $("<ul class='list-group'>");
+        let cardBody = $(`<div class="card-body">`);
+        cardBody.append(ingList);
+        ingCard.append(cardHeader, cardBody)
+        // create steps card
+
+        // make steps into an array
+        let newSteps = [];
+        let steps = recipe[0].steps.split("',")
+        steps.forEach(element => {
+            newSteps.push(element.replace("[", "").replace("'", "").replace("']", ""))
+        });
+        let name = $(`<h1>${recipe[0].name.toUpperCase()}</h1>`)
+        
+        let stepsCard = $(`<div class="card my-4">`);
+        let stepsCardHeader = $(`<div class="card-header">Ingredients:</div>`);
+        let stepsCardBody = $(`<div class="card-body">`);
+        
+        let stepsList = $("<ul class='list-group'>")
+        for (let index = 0; index < newIngredients.length; index++) {
+            ingList.append($(`<li class="list-group-item" id=${index}>${newIngredients[index]}</li>`));
+        }
+        for (let index = 0; index < newSteps.length; index++) {
+            stepsList.append($(`<li class="list-group-item" id=${index}>${newSteps[index]}</li>`));
+        }
+        
+        console.log(ingList);
+        
+        stepsCardBody.append(stepsList)
+        stepsCard.append(stepsCardHeader, stepsCardBody)
+        
+        $(".recipe-view").append(name, ingCard, stepsCard)
     })
 });
